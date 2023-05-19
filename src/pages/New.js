f7e88450562f23a6db921c7c975aef264c5feda1
jhsong <wngml203@naver.com>
2023-05-19 14:07:59 +0900

    feat New

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,9 +1,11 @@
+import DiaryEditor from "../components/DiaryEditor";
+
 const New = () => {
+
     return (
-    <div>
-        <h1>New</h1>
-        <p>이곳은 일기 추가 작성 페이지 입니다.</p>
-    </div>
-    );
-};
+        <div>
+            <DiaryEditor />
+        </div>
+    )
+}
 export default New;
